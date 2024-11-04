116accc7a039e7da5027e746fdd76a9c431d0cf2
albert <albertzhang.520@outlook.com>
2024-11-04 14:52:34 +0800

    add resources for module

--- kmp/src/commonMain/kotlin/KmpApp.kt
+++ kmp/src/commonMain/kotlin/KmpApp.kt
@@ -1,5 +1,7 @@
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -7,7 +9,11 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun KmpApp() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    Scaffold(modifier = Modifier
+        .statusBarsPadding()
+        .navigationBarsPadding()
+        .fillMaxSize()
+    ) { innerPadding ->
         Greeting(
             name = "Kmp",
             modifier = Modifier.padding(innerPadding)
